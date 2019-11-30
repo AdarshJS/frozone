6848a4a15afa0979d6bcc22e3259149174596e0a
AdarshJS <adarshjagan3895@gmail.com>
2019-11-30 16:17:07 -0500

    [src]: Added line to diplay current goal.

--- src/goal_pub.cpp
+++ src/goal_pub.cpp
@@ -15,11 +15,14 @@ ros::NodeHandle nh;
 ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/jackal/target/position", 1000);
 
 ros::Rate rate(10);
+float x = 4.0;
+float y = 4.0;
+std::cout << "Sending goal position X = " << x << "\t Y = " << y << std::endl;
 
 while(ros::ok()){
 geometry_msgs::Twist msg;
-msg.linear.x = 10.0;
-msg.linear.y = 10.0;
+msg.linear.x = x;
+msg.linear.y = y;
 msg.linear.z = 0.0;
 
 pub.publish(msg);
