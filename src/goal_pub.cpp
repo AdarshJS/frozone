d75a1e3ca89c074768ea5deae9b6ada20fd4ed2c
AdarshJS <adarshjagan3895@gmail.com>
2019-12-01 16:27:32 -0500

    [src/goal_pub]: Removed unnecessary lines.

--- src/goal_pub.cpp
+++ src/goal_pub.cpp
@@ -1,17 +1,10 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 
-// void filterVelocityCallback(const geometry_msgs::Twist& msg){
-// if (msg.angular.z > 0){
-// ROS_INFO_STREAM("Subscriber velocities:"<<" linear="<<msg.linear.x<<" angular="<<msg.angular.z);
-// }
-// }
-
 int main(int argc, char **argv){
 ros::init(argc, argv, "Goal_Pub");
 ros::NodeHandle nh;
 
-// ros::Subscriber sub = nh.subscribe("turtle1/cmd_vel",1000,&filterVelocityCallback);
 ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/jackal/target/position", 1000);
 
 ros::Rate rate(10);
@@ -26,7 +19,6 @@ msg.linear.y = y;
 msg.linear.z = 0.0;
 
 pub.publish(msg);
-// ROS_INFO_STREAM("Filtered velocities:"<<" linear="<<msg.linear.x<<" angular="<<msg.angular.z);
 rate.sleep();
 }
 
